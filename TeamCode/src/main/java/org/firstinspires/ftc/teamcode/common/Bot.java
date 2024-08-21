b373c67ca0c8b96acfa7fd5f40640d49fe80cf5e
zigathustra <chenders@gmail.com>
2024-08-21 15:50:46 -0400

    Update Bot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Bot.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.common;
 
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Bot extends Component {
