2715abc06c578e35b688310a87c1e45e0b9019fb
zigathustra <chenders@gmail.com>
2024-08-21 12:38:28 -0400

    Code for teleop control

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Bot.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.common;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Bot extends Component {
+
+    public Bot(Telemetry telemetry)
+    {
+        super(telemetry);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Component.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.common;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public abstract class Component {
+
+    protected Telemetry telemetry;
+
+    public Component(Telemetry telemetry)
+    {
+        this.telemetry = telemetry;
+    }
+}
