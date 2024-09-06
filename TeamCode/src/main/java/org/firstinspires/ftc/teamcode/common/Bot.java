3b3edc8ae8744336c36f193057978e929802c25c
hendersonc <hendersonc@trinityprep.org>
2024-09-06 13:10:02 -0400

    Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Bot.java
@@ -5,15 +5,12 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public abstract class Bot extends Component {
-    private final LiftOneMotor lift;
 //    private final LiftOneMotor lift;
 
     public Bot(HardwareMap hardwareMap, Telemetry telemetry) {
         super(telemetry);
-        lift = new LiftOneMotor(hardwareMap, telemetry);
  //       lift = new LiftOneMotor(hardwareMap, telemetry);
     }
-
 /*
     public void liftUp(double speed) {
         lift.liftUp(speed);
