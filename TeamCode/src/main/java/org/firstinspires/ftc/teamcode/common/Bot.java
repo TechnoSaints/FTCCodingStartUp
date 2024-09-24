42672368442fd7e4c41643d1dcd3e8df0019d325
hendersonc <hendersonc@trinityprep.org>
2024-09-24 11:10:56 -0400

    Refactoring actions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Bot.java
@@ -1,5 +1,9 @@
 package org.firstinspires.ftc.teamcode.common;
 
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -51,5 +55,57 @@ public abstract class Bot extends Component {
 
     public void liftZero() {
         lift.zero();
+    }
+
+
+    // Action classes and methods required to use scheduler
+    // Intended for use in auto opmodes, but could be used in teleop
+    public class OpenGrabber implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            telemetry.addData("Grabber Opening...",1);
+            telemetry.update();
+            return false;
+        }
+    }
+
+    public Action openGrabber() {
+        return new OpenGrabber();
+    }
+
+    public class CloseGrabber implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            telemetry.addData("Grabber Closing...",1);
+            telemetry.update();
+            return false;
+        }
+    }
+
+    public Action closeGrabber() {
+        return new CloseGrabber();
+    }
+
+    public class LiftToBottomPosition implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            telemetry.addData("Lift Moving to Bottom Position...",1);
+            telemetry.update();
+            return false;
+        }
+    }
+    public Action liftToBottomPosition() {
+        return new LiftToBottomPosition();
+    }
+    public class Shutdown implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            telemetry.addData("Shutting Bot Down...",1);
+            telemetry.update();
+            return false;
+        }
+    }
+    public Action shutdown() {
+        return new Shutdown();
     }
 }
