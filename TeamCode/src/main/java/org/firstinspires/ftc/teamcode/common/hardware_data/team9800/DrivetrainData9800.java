3ac3b20b6eccf826fa16fac8546eaed92c2e3cb6
hendersonc <hendersonc@trinityprep.org>
2024-10-02 13:02:41 -0400

    Refactoring

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware_data/team9800/DrivetrainData9800.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware_data/team9800/DrivetrainData9800.java
@@ -4,8 +4,8 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.common.hardware_data.DrivetrainData;
 
-public class DrivetrainData21528 extends DrivetrainData {
-    public DrivetrainData21528() {
+public class DrivetrainData9800 extends DrivetrainData {
+    public DrivetrainData9800() {
         maxNormalPower = 0.65;
         maxCreepPower = 0.25;
         leftFrontDirection = DcMotorSimple.Direction.FORWARD;
