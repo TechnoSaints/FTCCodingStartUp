7b33c633176fe53fd67d81936adc887b7ccc6fc4
hendersonc <hendersonc@trinityprep.org>
2024-10-02 11:21:15 -0400

    Refactoring Bot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/TeleopBot21528.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/TeleopBot21528.java
@@ -3,17 +3,10 @@ package org.firstinspires.ftc.teamcode.common;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-<<<<<<<< HEAD:TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/TeleopBot9800.java
-public class TeleopBot9800 extends Bot9800 {
-    private TeleopDrivetrain drivetrain = null;
-    public TeleopBot9800(HardwareMap hardwareMap, Telemetry telemetry)
-    {
-========
 public class TeleopBot21528 extends Bot21528 {
     private TeleopDrivetrain drivetrain = null;
 
     public TeleopBot21528(HardwareMap hardwareMap, Telemetry telemetry) {
->>>>>>>> Henderson:TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/TeleopBot21528.java
         super(hardwareMap, telemetry);
         drivetrain = new TeleopDrivetrain(hardwareMap, telemetry);
     }
