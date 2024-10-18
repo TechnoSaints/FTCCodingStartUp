8ed5e783065190d4061cbbf944a1f261e907b262
hendersonc <hendersonc@trinityprep.org>
2024-10-18 16:01:06 -0400

    Added 21527 files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Bot21527.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.common;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.common.hardware_data.GoBilda223DcMotorData;
+import org.firstinspires.ftc.teamcode.common.hardware_data.team21528.GrabberServoData21528;
+import org.firstinspires.ftc.teamcode.common.hardware_data.team21528.LiftData21528;
+
+public abstract class Bot21527 extends Component {
+
+    public Bot21527(HardwareMap hardwareMap, Telemetry telemetry) {
+        super(telemetry);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware_data/team21527/DrivetrainData21527.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.common.hardware_data.team21527;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.common.hardware_data.DrivetrainData;
+
+public class DrivetrainData21527 extends DrivetrainData {
+    public DrivetrainData21527() {
+        maxNormalPower = 0.5;
+        maxCreepPower = 0.25;
+        leftFrontDirection = DcMotorSimple.Direction.REVERSE;
+        leftBackDirection = DcMotorSimple.Direction.FORWARD;
+        rightFrontDirection = DcMotorSimple.Direction.REVERSE;
+        rightBackDirection = DcMotorSimple.Direction.FORWARD;
+    }
+}
