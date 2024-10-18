21e94465f9af4a1e0c43ff19fe070d5fa664c24c
hendersonc <hendersonc@trinityprep.org>
2024-10-18 14:40:28 -0400

    Update LiftData21528.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware_data/team21528/LiftData21528.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware_data/team21528/LiftData21528.java
@@ -5,7 +5,7 @@ import org.firstinspires.ftc.teamcode.common.hardware_data.LiftData;
 public class LiftData21528 extends LiftData {
     public LiftData21528() {
         maxMovePower = 0.65;
-        stopPower = 0.90;
+        stopPower = 0.95;
         maxPosition = 3000;
         maxTolerance = 25;
         minPosition = 0;
