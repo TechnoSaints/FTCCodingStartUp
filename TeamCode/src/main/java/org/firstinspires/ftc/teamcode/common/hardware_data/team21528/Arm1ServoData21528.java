035a3d0e642d9595a2d9dfc8ca479ea1d9d2a6eb
TechnoSaints <130092700+TechnoSaints@users.noreply.github.com>
2024-11-04 13:37:58 -0500

    grabber

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware_data/team21528/Arm1ServoData21528.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware_data/team21528/Arm1ServoData21528.java
@@ -4,8 +4,8 @@ import org.firstinspires.ftc.teamcode.common.hardware_data.ServoData;
 
 public class Arm1ServoData21528 extends ServoData {
     public Arm1ServoData21528() {
-        openPosition = 0.55;
-        closePosition = 0.25;
+        openPosition = 0.3;
+        closePosition = 0.85;
         middlePosition = 0.45;
     }
 }
