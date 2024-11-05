1cef44083e578b1f0c0d44b23fb903ab52b30e6b
TechnoSaints <130092700+TechnoSaints@users.noreply.github.com>
2024-11-05 12:22:45 -0500

    Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware_data/team21528/DrivetrainData21528_B.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware_data/team21528/DrivetrainData21528_B.java
@@ -7,7 +7,7 @@ import org.firstinspires.ftc.teamcode.common.hardware_data.DrivetrainData;
 public class DrivetrainData21528_B extends DrivetrainData {
     public DrivetrainData21528_B() {
         maxFastPower = 0.65;
-        maxMediumPower = 0.7;
+        maxMediumPower = 0.6;
         maxSlowPower = 0.25;
         leftFrontDirection = DcMotorSimple.Direction.REVERSE;
         leftBackDirection = DcMotorSimple.Direction.REVERSE;
