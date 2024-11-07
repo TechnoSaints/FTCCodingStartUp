f3b07589cae1a0094962dbd137896bae53364726
hendersonc <hendersonc@trinityprep.org>
2024-11-07 16:11:21 -0500

    Bot refactoring

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware_data/team21528/ArmServoData21528.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.common.hardware_data.team21528;
+
+import org.firstinspires.ftc.teamcode.common.hardware_data.ServoData;
+
+public class ArmServoData21528 extends ServoData {
+    public ArmServoData21528() {
+        openPosition = 0.16;
+        closePosition = 0.9;
+        middlePosition = 0.45;
+    }
+}
