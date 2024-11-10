6f29cfd5b10c66f917edc74eca07a69b25b4f542
hendersonc <hendersonc@trinityprep.org>
2024-11-10 13:32:34 -0500

    Config tweaks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware_data/team21528/Arm1ServoData21528.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware_data/team21528/Arm1ServoData21528.java
@@ -5,7 +5,7 @@ import org.firstinspires.ftc.teamcode.common.hardware_data.ServoData;
 public class Arm1ServoData21528 extends ServoData {
     public Arm1ServoData21528() {
         openPosition = 0.3;
-        closePosition = 0.96;
         middlePosition = 0.5;
+        closePosition = 0.97;
     }
 }
