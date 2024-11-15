9efc7dc6c29b6e8f087a222e05f25497d3c9dc89
TechnoSaints <130092700+TechnoSaints@users.noreply.github.com>
2024-11-15 08:02:06 -0500

    Update GrabberServoData21528.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware_data/team21528/GrabberServoData21528.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware_data/team21528/GrabberServoData21528.java
@@ -5,6 +5,6 @@ import org.firstinspires.ftc.teamcode.common.hardware_data.ServoData;
 public class GrabberServoData21528 extends ServoData {
     public GrabberServoData21528() {
         openPosition = 0.43;
-        closePosition = 0.85;
+        closePosition = 0.9;
     }
 }
